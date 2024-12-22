f5e44f7482cf72c87329da2684cbcf5753a1fbe6
bhchen2001 <bhchen312551074.cs12@nycu.edu.tw>
2024-12-22 19:34:59 +0800

    feat: change IPC from pipe to msg queue

--- include/comm.h
+++ include/comm.h
@@ -8,30 +8,38 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <sys/un.h>
+#include <sys/msg.h>
 #include <errno.h>
 
+#define LOW_PRIORITY_MSGQ_KEY 1234
+#define HIGH_PRIORITY_MSGQ_KEY 5678
 #define BUFFER_SIZE 1024
 #define PORT 4444
 
 /*
- * pipe descriptors
+ * file descriptors for server and client
  */
-extern int high_priority_pipe[2];
-extern int low_priority_pipe[2];
+extern int server_fd;
 extern int client_fd;
+
+extern int high_priority_msgq;
+extern int low_priority_msgq;
 
 /*
  * client request structure
  */
 typedef struct {
+    long msg_type;
     int client_id;
     char request_type[10];
     int control_signal;
     int priority;
 } client_request;
 
-void initialize_pipes();
-void setup_server(int *server_fd, int *client_fd);
-void handle_client_requests(int client_fd);
+void initialize_msgq();
+void remove_msgq();
+void setup_server();
+void handle_client_requests();
+void close_fd();
 
 #endif
--- src/comm.c
+++ src/comm.c
@@ -1,21 +1,33 @@
-#include "comm.h"
+#include "include/comm.h"
 
-int high_priority_pipe[2];
-int low_priority_pipe[2];
+int high_priority_msgq, low_priority_msgq;
 
-void initialize_pipes() {
-    if (pipe(high_priority_pipe) < 0 || pipe(low_priority_pipe) < 0) {
-        perror("Pipe creation failed");
+int server_fd = -1;
+int client_fd = -1;
+
+void initialize_msgq() {
+    high_priority_msgq = msgget(HIGH_PRIORITY_MSGQ_KEY, 0666 | IPC_CREAT);
+    low_priority_msgq = msgget(LOW_PRIORITY_MSGQ_KEY, 0666 | IPC_CREAT);
+
+    if (high_priority_msgq < 0 || low_priority_msgq < 0) {
+        perror("Message queue creation failed");
         exit(EXIT_FAILURE);
     }
 }
 
-void setup_server(int *server_fd, int *client_fd) {
+void remove_msgq() {
+    msgctl(high_priority_msgq, IPC_RMID, NULL);
+    msgctl(low_priority_msgq, IPC_RMID, NULL);
+
+    return;
+}
+
+void setup_server() {
     struct sockaddr_in server_addr;
     struct sockaddr_in client_addr;
     int client_addr_len = sizeof(client_addr);
 
-    if ((*server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         perror("Socket creation failed");
         exit(EXIT_FAILURE);
     }
@@ -24,27 +36,27 @@ void setup_server(int *server_fd, int *client_fd) {
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(PORT);
 
-    if (bind(*server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
         perror("Bind failed");
         exit(EXIT_FAILURE);
     }
 
-    if (listen(*server_fd, 5) < 0) {
+    if (listen(server_fd, 5) < 0) {
         perror("Listen failed");
         exit(EXIT_FAILURE);
     }
 
     printf("Server listening on port %d\n", PORT);
 
-    if ((*client_fd = accept(*server_fd, (struct sockaddr *)&client_addr, (socklen_t *)&client_addr_len)) < 0) {
+    if ((client_fd = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t *)&client_addr_len)) < 0) {
         perror("Accept failed");
         exit(EXIT_FAILURE);
     }
 
-    printf("New client connected: %d\n", *client_fd);
+    printf("New client connected: %d\n", client_fd);
 }
 
-void handle_client_requests(int client_fd) {
+void handle_client_requests() {
     char buffer[BUFFER_SIZE] = {0};
     client_request request;
 
@@ -53,7 +65,10 @@ void handle_client_requests(int client_fd) {
         memset(&request, 0, sizeof(client_request));
         request.priority = -1;
 
-        read(client_fd, buffer, BUFFER_SIZE);
+        if (read(client_fd, buffer, BUFFER_SIZE) <= 0) {
+            perror("Client Disconnect");
+            return;
+        }
 
         // parse client request
         char request_type[10];
@@ -67,15 +82,24 @@ void handle_client_requests(int client_fd) {
         request.priority = (strcmp(request_type, "CONTROL") == 0) ? 1 : 0;
 
         if (request.priority == 1) {
-            write(high_priority_pipe[1], &request, sizeof(client_request));
+            request.msg_type = 1;
+            if (msgsnd(high_priority_msgq, &request, sizeof(client_request) - sizeof(long), 0) < 0) {
+                perror("Message send failed");
+                exit(EXIT_FAILURE);
+            }
         } else {
-            write(low_priority_pipe[1], &request, sizeof(client_request));
+            request.msg_type = 1;
+            if (msgsnd(low_priority_msgq, &request, sizeof(client_request) - sizeof(long), 0) < 0) {
+                perror("Message send failed");
+                exit(EXIT_FAILURE);
+            }
         }
-
-        // char response[BUFFER_SIZE];
-        // snprintf(response, BUFFER_SIZE, "Request received: %s %d\n", request_type, signal);
-        // write(client_fd, response, BUFFER_SIZE);
-
-        // close(client_fd);
     }
+}
+
+void close_fd() {
+    close(server_fd);
+    close(client_fd);
+
+    return;
 }
