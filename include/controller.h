095e4a0de79437c90f85edf2fe2fd6e34b5d6f93
bhchen2001 <bhchen312551074.cs12@nycu.edu.tw>
2024-12-23 23:29:22 +0800

    feat: remove comm shared info and add pump mutex

--- include/controller.h
+++ include/controller.h
@@ -7,17 +7,22 @@
 #include <sys/resource.h>
 #include <time.h>
 #include <signal.h>
+#include <pthread.h>
 
 #include "comm.h"
 #include "shm.h"
 
-#define LOW_PRIORITY 10
-#define HIGH_PRIORITY -10
+#define SCHED_FIFO_LOW_PRIORITY 10
+#define SCHED_FIFO_MED_PRIORITY 20
+#define SCHED_FIFO_HIGH_PRIORITY 30
 
 extern int shm_id;
 extern shared_humidity *shm_ptr;
 
-void high_priority_task();
-// void sigusr1_handler();
-void sigusr2_handler();
-void low_priority_task();
+extern pthread_mutex_t water_pump_mutex;
+
+void busy_wait(int seconds);    // busy wait for priority settng testing
+void *high_priority_task();     // high priority task (control water pump from user & send alarm)
+void sigusr1_handler();         // signal handler for SIGUSR1 (automatic control of water pump)
+void *low_priority_task();      // low priority task (query soil moisture)
+void water_pump_mutex_init();   // initialize water pump mutex
--- src/controller.c
+++ src/controller.c
@@ -2,85 +2,138 @@
 
 int shm_id;
 shared_humidity *shm_ptr;
+pthread_mutex_t water_pump_mutex;
 
-void high_priority_task() {
-    printf("[High-Priority] Started\n");
-    // set higher priority
-    setpriority(PRIO_PROCESS, 0, HIGH_PRIORITY);
-    client_request request;
+void busy_wait(int seconds) {
+    struct timespec start, current;
+    clock_gettime(CLOCK_THREAD_CPUTIME_ID, &start);
+    while (1) {
+        clock_gettime(CLOCK_THREAD_CPUTIME_ID, &current);
+        double elapsed = (current.tv_sec - start.tv_sec) + (current.tv_nsec - start.tv_nsec) / 1e9;
+        if (elapsed >= seconds) break;
+    }
+}
+
+void *high_priority_task() {
+    printf("[HIGH_PRIORITY] Started\n");
+    task_request request;
+    memset(&request, 0, sizeof(task_request));
 
     while (1) {
-        if (msgrcv(high_priority_msgq, &request, sizeof(client_request) - sizeof(long), 0, 0) < 0) {
+        if (msgrcv(high_priority_msgq, &request, sizeof(task_request) - sizeof(long), 0, 0) < 0) {
             perror("Message receive failed");
             exit(EXIT_FAILURE);
         }
-        printf("[High-Priority] Handling CONTROL request: Client=%d, Signal=%d\n", request.client_id, request.control_signal);
+        printf("[HIGH_PRIORITY] Handling CONTROL request: Client=%d, Signal=%d ,Period=%d\n", request.client_id, request.control_signal, request.pump_period);
 
-        /*
-         **********************************************************************
-         * simulate controlling water pump (integrated with actuator-subsystem)
-         */
         char response[BUFFER_SIZE];
-        // ********************************************************************
 
         // send response to client
-        if (strcmp(request.request_type, "ALARM") == 0) {
-            printf("[High-Priority] Sending alarm response to client\n");
-            sprintf(response, "ALARM: Humidity below threshold\n");
+        if (strcmp(request.request_type, ALARM) == 0) {
+            printf("[HIGH_PRIORITY] Sending alarm response to client\n");
+            sprintf(response, "ALARM: Humidity is below threshold: %.2f%%\n", request.humidity);
             if (write(client_fd, response, BUFFER_SIZE) < 0) {
-                perror("Write failed");
+                perror("[HIGH_PRIORITY] Write failed");
                 exit(EXIT_FAILURE);
             }
-        } else {
-            printf("[High-Priority] Sending response to client\n");
-            sprintf(response, "Water pump is %s\n", request.control_signal ? "ON" : "OFF");
+        } 
+        else if (strcmp(request.request_type, CONTROL) == 0) {
+            printf("[HIGH_PRIORITY] Controlling water pump:");
+            sprintf(response, "Water pump is now");
+            switch (request.control_signal) {
+                case PUMP_OFF:
+                    printf(" OFF\n");
+                    strcat(response, " OFF\n");
+                    break;
+                case PUMP_ON:
+                    printf(" ON\n");
+                    strcat(response, " ON\n");
+                    break;
+                case PUMP_PERIOD:
+                    printf(" PERIOD: %d\n", request.pump_period);
+                    sprintf(response, " ON with period: %d\n", request.pump_period);
+                    break;
+            }
+            // lock the water pump mutex
+            pthread_mutex_lock(&water_pump_mutex);
+            // busy wait for simulating water pump control
+            busy_wait(5);
+            // unlock the water pump mutex
+            pthread_mutex_unlock(&water_pump_mutex);
+
+            printf("[HIGH_PRIORITY] Sending response to client\n");
             if (write(request.client_id, response, BUFFER_SIZE) < 0) {
-                perror("Write failed");
+                perror("[HIGH_PRIORITY] Write failed");
                 exit(EXIT_FAILURE);
             }
         }
+        else {
+            printf("[HIGH_PRIORITY] Invalid request type\n");
+        }
     }
+    return NULL;
 }
 
-void low_priority_task() {
-    printf("[Low-Priority] Started\n");
-
-    // set lower priority
-    setpriority(PRIO_PROCESS, 0, LOW_PRIORITY);
-    client_request request;
+void *low_priority_task() {
+    printf("[LOW_PRIORITY] Started\n");
+    task_request request;
+    memset(&request, 0, sizeof(task_request));
 
     while (1) {
-        if (msgrcv(low_priority_msgq, &request, sizeof(client_request) - sizeof(long), 0, 0) < 0) {
-            perror("[Low-Priority] Message receive failed");
+        if (msgrcv(low_priority_msgq, &request, sizeof(task_request) - sizeof(long), 0, 0) < 0) {
+            perror("[LOW_PRIORITY] Message receive failed");
             exit(EXIT_FAILURE);
         }
-        printf("[Low-Priority] Handling QUERY request: Client=%d\n", request.client_id);
+        if (strcmp(request.request_type, QUERY) == 0) {
+            printf("[LOW_PRIORITY] Handling QUERY request: Client=%d\n", request.client_id);
 
-        /*
-         ******************************************************************
-         * simulate fetching sensor data (integrated with sensor-subsystem)
-         */
+            // get humidity from shared memory
+            float humidity;
+            get_humidity((void *)shm_ptr, &humidity);
+            printf("[LOW_PRIORITY] Current Humidity: %.2f%%\n", humidity);
 
-        // get humidity from shared memory
-        float humidity;
-        get_humidity((void *)shm_ptr, &humidity);
+            // simulate query overhead
+            busy_wait(2);
 
-        printf("[Low-Priority] Current Humidity: %.2f%%\n", humidity);
-        // ****************************************************************
-
-        // send response to client
-        char response[BUFFER_SIZE];
-        sprintf(response, "Current Humidity: %.2f%%\n", humidity);
-        if (write(request.client_id, response, BUFFER_SIZE) < 0) {
-            perror("Write failed");
-            exit(EXIT_FAILURE);
+            // send response to client
+            char response[BUFFER_SIZE];
+            sprintf(response, "Current Humidity: %.2f%%\n", humidity);
+            if (write(request.client_id, response, BUFFER_SIZE) < 0) {
+                perror("[LOW_PRIORITY] Write failed");
+                exit(EXIT_FAILURE);
+            }
         }
     }
+
+    return NULL;
 }
 
-void sigusr2_handler() {
+void sigusr1_handler() {
     // control the water pump
-    printf("[Controller] Controlling water pump\n");
+    printf("[CONTROLLER] Controlling water pump\n");
 
+    // lock the water pump mutex
+    pthread_mutex_lock(&water_pump_mutex);
+    busy_wait(5);
+    // unlock the water pump mutex
+    pthread_mutex_unlock(&water_pump_mutex);
+
+    return;
+}
+
+void water_pump_mutex_init() {
+    pthread_mutexattr_t attr;
+    pthread_mutexattr_init(&attr);
+
+    // set mutex as process-shared (optional for multi-process scenarios)
+    pthread_mutexattr_setpshared(&attr, PTHREAD_PROCESS_PRIVATE);
+
+    // enable priority inheritance
+    pthread_mutexattr_setprotocol(&attr, PTHREAD_PRIO_INHERIT);
+
+    // initialize the mutex
+    pthread_mutex_init(&water_pump_mutex, &attr);
+
+    pthread_mutexattr_destroy(&attr);
     return;
 }
