eb598b4d56817d93c2f75c923c8d13916f1d7960
Bo Han Chen <bhchen312551074.cs12@nycu.edu.tw>
2024-12-26 15:04:15 +0800

    feat: add button GPIO utils

--- include/device_utils.h
+++ include/device_utils.h
@@ -4,12 +4,19 @@
 #define L298N_DEV   "/dev/l298n"
 #define GPIO_CHIP_DEV   "/dev/gpiochip0"
 
+#define GPIO_PUMP_ON 23
+#define GPIO_PUMP_OFF 24
+
 #define DEV_PUMP_ON 'O'
 #define DEV_PUMP_OFF 'S'
 
+#define HUMIDITY_THRESHOLD_LOW 30.0
+#define HUMIDITY_THRESHOLD_HIGH 60.0
+
 void open_ads1115_device(int *fd_ads);
 void open_l298n_device(int *fd_l298n);
+void open_gpio_device(int *gpio_fd);
 
 void get_humidity_dev(int fd_ads, float *humidity);
-
 void set_pump(int fd_l298n, char command);
+void read_gpio(int gpio_fd, int line, int *value);
--- src/device_utils.c
+++ src/device_utils.c
@@ -4,6 +4,12 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <linux/gpio.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
 
 void open_ads1115_device(int *fd_ads) {
     *fd_ads = open(ADS1115_DEV, O_RDONLY);
@@ -23,6 +29,15 @@ void open_l298n_device(int *fd_l298n) {
     return;
 }
 
+void open_gpio_device(int *gpio_fd) {
+    *gpio_fd = open(GPIO_CHIP_DEV, O_RDONLY);
+    if (*gpio_fd < 0) {
+        perror("open gpiochip0");
+        exit(EXIT_FAILURE);
+    }
+    return;
+}
+
 void get_humidity_dev(int fd_ads, float *humidity) {
     char data[32];
     ssize_t r = read(fd_ads, data, sizeof(data) - 1);
@@ -30,7 +45,15 @@ void get_humidity_dev(int fd_ads, float *humidity) {
         data[r] = '\0';
         int raw_value = atoi(data);
 
-        *humidity = (raw_value * 4.096f) / 32768.0f;
+        double voltage = ((double)raw_value * 4.096f) / 32767.0;
+        if (voltage < 0) voltage = 0;
+        if (voltage > 5) voltage = 5;
+
+        double h_tmp = (1 - (voltage / 5)) * 100;
+        if (h_tmp < 0) h_tmp = 0;
+        if (h_tmp > 100) h_tmp = 100;
+
+        *humidity = (float)h_tmp;
         printf("[SENSOR] Current Humidity: %.2f%%\n", *humidity);
     } else {
         perror("Failed to read data");
@@ -44,5 +67,32 @@ void set_pump(int fd_l298n, char command) {
         perror("Failed to write to device");
         exit(EXIT_FAILURE);
     }
+    return;
+}
+
+void read_gpio(int gpio_fd, int line, int *value) {
+    struct gpiohandle_request req;
+    struct gpiohandle_data data;
+    int ret;
+
+    req.lineoffsets[0] = line;
+    req.lines = 1;
+    req.flags = GPIOHANDLE_REQUEST_INPUT;
+
+    ret = ioctl(gpio_fd, GPIO_GET_LINEHANDLE_IOCTL, &req);
+    if (ret < 0) {
+        perror("Failed to get line handle");
+        exit(EXIT_FAILURE);
+    }
+
+    ret = ioctl(req.fd, GPIOHANDLE_GET_LINE_VALUES_IOCTL, &data);
+    if (ret < 0) {
+        perror("Failed to get line values");
+        exit(EXIT_FAILURE);
+    }
+
+    close(req.fd);
+
+    *value = data.values[0];
     return;
 }
